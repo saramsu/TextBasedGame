dcad0793de8c1d548d4bd24deee948960d1de561
Sara Amaya <98931088+saramsu@users.noreply.github.com>
2022-10-20 00:23:36 -0500

    save2

--- textBasedGame.py
+++ textBasedGame.py
@@ -1,2 +1,11 @@
-print("Hi there! Welcome to the adventure. Let's start wit your name")
+#Possible reponds 
+AnswerA = ["A", "a"]
+AnswerB = ["B", "b"]
+AnswerC = ["C", "c"]
+
+#The story starts with the section named "one"
+def one():
+print('Hi there! Welcome to the adventure.')
 name = input('Enter your preferred name:')
+print('Welcome to the adventure + name+!')
+print('You have been driven during the entire night and suddenly a girl with long and disavled hair ask you to carry her to the neares village. What are you going to do?')
